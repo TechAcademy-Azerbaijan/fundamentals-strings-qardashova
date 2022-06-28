b8ace197dbfa5580b27c7bee3ffe429fae1ab695
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-28 09:45:59 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -5,5 +5,16 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
-  
+    let n = result.input;
+  let a = n.split(',')[0];
+  let birinci = a[0];
+  let ikinci = a[a.length-1];
+  if (birinci > ikinci) {
+    console.log(birinci);
+  }else if (ikinci > birinci) {
+    console.log(ikinci);
+  }
+  else{
+    console.log("=");
+  }
 });
