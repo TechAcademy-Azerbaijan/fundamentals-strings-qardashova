d3604fea8399b275b1bcf6d9973a41daab5d9135
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-28 10:50:48 +0300

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -5,5 +5,15 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+      let n = result.input;
+    let array = n.split(" ");
+    console.log(array);
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] != "") {
+        count++;
+      }   
+    }
+    console.log(count);
   
 });
