9853dc1382449c3393360d90d9cf00d4bb85f9f5
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-28 10:01:55 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -5,5 +5,14 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    let n = result.input;
+    let a = n.split(',')[0];
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (n[i] == "!" || n[i] == ".") {
+        count++
+      }
+    }
+    console.log(count);
   
 });
