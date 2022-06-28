db02cacc9c175d6024a2bc64efbdb3b4514c798b
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-28 09:52:16 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -5,5 +5,25 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let n = result.input;
+  let a = n.split(',')[0];
+  let count2 = 0;
+  let count5 = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] == "2") {
+      count2++
+    }
+    if (n[i] == "5") {
+      count5++
+    }
+  }
+  if (count2 > count5) {
+    console.log("2");
+  }else if (count5 > count2) {
+    console.log("5");
+  }
+  else{
+    console.log("=");
+  }
   
 });
