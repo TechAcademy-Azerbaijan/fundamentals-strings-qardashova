ddd18078f6f8150e34599c20bf087c658045b9da
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-30 19:27:56 +0300

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -5,5 +5,23 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
-  
+      var n = result.input;
+    var array = [];
+    var nope = -1;
+    var count = 0;
+    for (let i = 0; i < n.length; i++) {
+      if (n[i] == ' ') {
+        array.push(i);
+      }
+     else{
+        count++;
+        if (count == n.length) {
+          console.log(nope);
+        }
+     }
+    }
+    if (array.length > 1) {
+      console.log(Math.min(...array));
+      console.log(Math.max(...array));
+    }
 });
