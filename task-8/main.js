704829816dfae59f4c77f29c91ccdabea29493fc
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-30 17:53:17 +0300

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -5,5 +5,17 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+    var n = result.input;
+    var array = n.split("");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i == 0 ) {
+        array[i] = array[i].toUpperCase();
+      }
+      else if( array[i] == " "){
+        array[i+1] = array[i+1].toUpperCase();
+      }
+    }
+    console.log(array.join(''));
   
 });
