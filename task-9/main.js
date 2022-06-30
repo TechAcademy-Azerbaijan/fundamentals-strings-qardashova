13f4de0c72110fb28e7890c75a12d1d5ebf2391f
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-30 18:20:27 +0300

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,6 +1,9 @@
-// Use these variables in your code
-
-let s = "This is a cat sitting on a table"
-let c = "t"
-
-// Write code here. 
+    let s = "This is a cat sitting on a table"
+    let c = "t"
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] == c || s[i] == c.toUpperCase()) {
+        count++;
+      } 
+    }
+    console.log(count);
